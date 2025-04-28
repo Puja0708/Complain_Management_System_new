a9992ce09fa33e744229c22ab04b4eaddc7a7a4f
Puja Singh <pujasingh@Pujas-MacBook-Air.local>
2025-04-28 17:08:36 +0530

    collectstatic

--- Complain_Management_System/settings.py
+++ Complain_Management_System/settings.py
@@ -127,6 +127,7 @@ USE_TZ = True
 #     # '/CMS_Project/Complain_Management_System/complaints/static/complaints/',
 # ]
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
